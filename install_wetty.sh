8e5b76478a91c6cda5b774df1551fc4b78f1aaa8
yayworks <raj@yayworks.com>
2017-05-26 11:57:37 -0500

    Update install_wetty.sh

--- install_wetty.sh
+++ install_wetty.sh
@@ -45,16 +45,16 @@ Y
 EOF
 rm ibm-6.9.1.0-node-v6.9.1-linux-ppc64le.bin
 #
-export PATH=/usr/local/node/bin:/usr/local/cuda/bin:$PATH
+#export PATH=/usr/local/node/bin:/usr/local/cuda/bin:$PATH
 ##Install wetty
-mkdir -p /home/nimbix/wetty
-cd /home/nimbix
+#mkdir -p /home/nimbix/wetty
+#cd /home/nimbix
 
-git clone https://github.com/krishnasrinivas/wetty
+#git clone https://github.com/krishnasrinivas/wetty
 
-cd wetty
+#cd wetty
 
-/usr/local/node/bin/npm install
+#/usr/local/node/bin/npm install
 
 exit 0
 
