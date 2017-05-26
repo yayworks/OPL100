dcd4136e275a8434d4913d0d5063ecb5e8632cc5
yayworks <raj@yayworks.com>
2017-05-26 10:56:19 -0500

    Update install_wetty.sh

--- install_wetty.sh
+++ install_wetty.sh
@@ -1,20 +1,21 @@
+
 #!/bin/bash
 ###############################################################################
 #                                                                             #
-# yb-config.NIMBIX.ppc64le.p8AI.sh - Configuraton of the PowerAI GPU Lab      #
+# install_wetty.sh - Install Web TTY                                          #
 #                                                                             #
 # Copyright (C) 2017 Yayworks, Inc. - All Rights Reserved                     #
 #                                                                             #
-# Last revised 05/25/2017                                                     #
+# Last revised 05/26/2017                                                     #
 #                                                                             #
 ###############################################################################
         display_usage() {
                 echo "Run this script as ssh_user from /home/ssh_user directory"
-                echo "Install nodejs SDK in /usr/local/node"
+                echo "Install Web TTY"
                 }
 ###############################################################################
 
-echo "Pre configuring this system as a Basic Yaybench POWER8/GPU Lab..."
+echo "Installing Web TTY..."
 
 
 wget https://s3.amazonaws.com/yb-lab-cfg/ibm-6.9.1.0-node-v6.9.1-linux-ppc64le.bin
@@ -44,27 +45,9 @@ Y
 EOF
 rm ibm-6.9.1.0-node-v6.9.1-linux-ppc64le.bin
 #
-echo "modify $PATH in .bashrc; add the following line to .bashrc before sourcing it"
-
-echo 'export PATH=/usr/local/node/bin:/usr/local/cuda/bin:/opt/ibm/xlC/13.1.5/bin:/opt/ibm/xlf/15.1.5/bin:$PATH' >> .bashrc
-echo 'export PATH=/usr/local/node/bin:/usr/local/cuda/bin:/opt/ibm/xlC/13.1.5/bin:/opt/ibm/xlf/15.1.5/bin:$PATH' >> /etc/bash.bashrc
-export PATH=/usr/local/node/bin:/usr/local/cuda/bin:/opt/ibm/xlC/13.1.5/bin:/opt/ibm/xlf/15.1.5/bin:$PATH
-
-username="user"
-password="user"
-                pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
-                sudo useradd -m -p $pass $username
-
-#echo "Configuring this system as a Basic Yaybench POWER8/GPU Lab..."
-
-##Set up login-based access to the system
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-
-sudo service ssh restart
 
 ##Install wetty
-cd /home/nimbix
-mkdir wetty
+mkdir -p wetty
 
 git clone https://github.com/krishnasrinivas/wetty
 
@@ -72,13 +55,5 @@ cd wetty
 
 /usr/local/node/bin/npm install
 
-##Programmatically create self-signed certificates
-/usr/bin/yb-gencert yay
-
-##Start wetty
-#sudo /usr/local/node/bin/node app.js --sslkey yay.key --sslcert yay.pem -p 3000 &
-
-
 exit 0
-
 
